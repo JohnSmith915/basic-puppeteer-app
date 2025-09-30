1dca1dd2c349efffb50c933310c20a40f4ab06fd
JohnSmith915 <johnsmithacctesttemp@gmail.com>
2025-09-30 18:07:38 +0800

    disable browser timeout

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const app = express();
         args: ["--single-process", "--no-sandbox", "--disable-setuid-sandbox", "--disable-gpu", "--no-zygote", "--disable-dev-shm-usage"],
         disableXvfb: true,
         executablePath: "./google-chrome-stable",
+        timeout: 0,
     });
     console.log("browser started");
 
